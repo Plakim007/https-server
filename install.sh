3737754283227ac746163bd862795d90281030a4
Plakim007 <31442234+Plakim007@users.noreply.github.com>
2024-02-19 18:55:29 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+apt-get update -y
+apt-get upgrade -y
+apt install python3 -y
+apt install figlet -y
+apt install nodejs -y
+npm install http-server -g
+
+chmod +x run-node
+chmod +x edit-miner
+
+mv run-node /data/data/com.termux/files/usr/bin
+mv edit-miner /data/data/com.termux/files/usr/bin
+
+cd /data/data/com.termux/files/usr/bin
+
+chmod +x run-node
+chmod +x edit-miner
+
+cd && cd /data/data/com.termux/files/usr/etc
+nano bash.bashrc
